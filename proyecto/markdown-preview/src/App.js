9f603feac3e265110ede92e67d8092439fe20a53
jhonatanquirama <quiramapino47@gmail.com>
2020-08-29 14:24:11 -0500

    actualizaciones

---
+++ proyecto/markdown-preview/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Mark from "./Components/test"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Mark/>
+    </div>
+  );
+}
+
+export default App;
